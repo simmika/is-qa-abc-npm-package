8e3d9cfb76e44f68a8c12778cbf13682b539b784
simmika <simonam@wix.com>
2023-12-20 10:55:35 +0200

    static text

--- package/index.js
+++ package/index.js
@@ -6,10 +6,15 @@
 //   return typeof int === 'number'
 // }
 
-function sum(a, b) {
-  return a + b
+// function sum(a, b) {
+//   return a + b
+// }
+
+function staticText() {
+  return 'static text'
 }
 
 // module.exports = isAbc
 // module.exports = isInt
-module.exports = sum
+// module.exports = sum
+module.exports = staticText
